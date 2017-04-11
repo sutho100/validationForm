47ea5c7584d8e60ba77fd94aef60de60e796815c
Todd Sutherland <sutho100@gmail.com>
2017-04-11 10:33:57 +1000

    github backup

---
+++ js/main.js
@@ -0,0 +1,37 @@
+﻿//$("#commentForm").validate();
+$(document).ready(function() {
+    $('#commentForm').validate({ // initialize the plugin
+         
+       
+        rules: {
+            rating: {
+                selectcheck: true
+            },
+            howFind: {
+                required: true
+            },
+            howRate: {
+                required: true 
+            }
+        }
+    });
+    jQuery.validator.addMethod('selectcheck', function(value) {
+        return (value != '0');
+    }, "please rate us");
+   
+    
+});
+
+$('#selectRadio1 input[name=radio]').change(function () {
+    if (!$('#subscribe').is(":checked")) {
+        
+        $subscribeOptionsCheckBoxes = $('#subscribeOptions input[type=checkbox]');
+        $subscribeOptionsCheckBoxes.prop('checked', false);
+        $subscribeOptionsCheckBoxes.attr('disabled', 'disabled');
+        
+    } else {
+       
+        $subscribeOptionsCheckBoxes.removeAttr('disabled');
+    }
+    
+});
